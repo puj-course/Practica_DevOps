7a3ffdc5847051f238401b3ab05f57bd3d27b847
JuanParias29 <juanpabloarias2906@gmail.com>
2025-03-19 23:05:25 -0500

    ✨ feat: creacion del test base

--- src/test/java/CLUB/de/Superheroes/ClubHeroes/HeroServiceTest.java
+++ src/test/java/CLUB/de/Superheroes/ClubHeroes/HeroServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 import java.util.List;
